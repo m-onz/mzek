fb805cd5d713abdb06a5f1f21ee8699ae12c71b5
m-onz <monz@startmail.com>
2021-07-18 18:00:32 +0100

    fix development script

--- dev.sh
+++ dev.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "starting mzek-server"
 node server &
+sleep 3
+echo "starting mzek-client"
 node client &
-cd /modules/auditd
+echo "starting auditd module"
 sudo ls
+cd ./modules/auditd/
 ./injest.sh
