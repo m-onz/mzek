9ecb165c914e5ffa310f49f76587bda6c967d931
m-onz <monz@startmail.com>
2021-07-30 17:12:49 +0100

    tshark monitor

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
 
 # `MzEK`
 
-## about
-
-experimental p2p loghost system build on secure scuttlebutt (ssb)
-
 ## status
 
 * not ready!
-* under active development
-
-## modules
-
-So far modules are just ssb-clients that append messages to the nodes feed.
-
-* logfeed: injest a continous log stream
-* tripwire: get an event if a commands output changes
 
 ## example config file
 
@@ -31,37 +19,9 @@ tripwires:
   services: systemctl list-units --type service --state running --plain
   listeningports: netstat -tunlp
 
-# these are streams of continuos data
+# these are streams of continuous data
 logfeeds:
   tcpdump: tcpdump -q -i wlp3s0
   auditd: tail -f /var/log/audit/audit.log
-
-```
-
-## notes
-
-For development and testing I'm just making sure not to run as root and
- this requires allowing a non-root user to use tcpdump and read auditd
- logs.
-
-I am planning to lock this down using an mzek group that has permissions
- to do what is needed. This will be limited and possibly run within a chroot
- jail or selinux jail.
-
-```sh
-
-# allow tcpdump to be run as a non-root user:
-
-  sudo chown tcpdump:tcpdump /usr/sbin/tcpdump
-  sudo chmod 755 /usr/sbin/tcpdump
-  tcpdump -X
-  ll /usr/sbin/tcpdump
-  sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-  sudo ln -s /usr/sbin/tcpdump /usr/local/bin/tcpdump
-
-# allow auditd log to be run as a non-root user:
-
-  sudo chown monz -R /var/log/audit/audit
-  sudo chgrp monz -R /var/log/audit/audit
 
 ```
--- modules/mon/tshark.js
+++ modules/mon/tshark.js
@@ -1,12 +1,53 @@
-// sudo tshark -i enp2s0f1 -T json -q | node index.js
+// tshark -N n -T fields -e ip.src_host -e tcp.srcport -e ip.dst_host -e tcp.dstport -i enp2s0f1 -T json -q | node tshark.js
 
 var JSONStream = require('JSONStream')
 var es = require('event-stream')
+var os = require('os')
+var dns = require('dns')
+
+var k = Object.keys(os.networkInterfaces())[1]
+var interface = os.networkInterfaces()[k][0]
+var my_ip = interface.address
+
+console.log(my_ip)
+
+var remote_hosts = []
 
 var uid = parseInt(process.env.SUDO_UID)
 if (uid) process.setuid(uid)
 //console.log('running as ', process.getuid())
 
 process.stdin.pipe(JSONStream.parse('*'))
-  .pipe(es.mapSync(function (a) { console.log(JSON.stringify(a, null, 2)); }))
-
+  .pipe(es.mapSync(function (a) {
+    var layers = a._source.layers
+    console.log('.. ', layers, ' ..')
+    // if (layers.hasOwnProperty('ip.src_host') &&
+    //     layers.hasOwnProperty('tcp.srcport')
+    // ) {
+    //   var src_host = layers['ip.src_host'][0]
+    //   var src_port = layers['tcp.srcport'][0]
+    //   if (src_host &&
+    //     my_ip !== src_host &&
+    //     !remote_hosts.includes(src_host)) {
+    //       dns.lookupService(src_host, parseInt(src_port), function (er, hostname) {
+    //        if (!er) remote_hosts.push({ hostname: hostname, ip: src_host, port: src_port })
+    //          else remote_hosts.push({ hostname: null, ip: src_host, port: src_port })
+    //       })
+    //     }
+    // }
+    // if (layers.hasOwnProperty('ip.dst_host') &&
+    //     layers.hasOwnProperty('tcp.dstport')
+    // ) {
+    //   var dst_host = layers['ip.dst_host'][0]
+    //   var dst_port = layers['tcp.dstport'][0]
+    //   if (dst_host &&
+    //     my_ip !== dst_host &&
+    //    !remote_hosts.includes(dst_host)) {
+    //      dns.lookupService(dst_host, parseInt(dst_port), function (er, hostname) {
+    //       if (!er) remote_hosts.push({ hostname: hostname, ip: dst_host, port: dst_port })
+    //         else remote_hosts.push({ hostname: null, ip: dst_host, port: dst_port })
+    //      })
+    //    }
+    // }
+    // console.log(remote_hosts)
+}))
